5babf36bab1a218a4fc4778a9f337417f46cc57e
anon7215 <anon7215@users.noreply.github.com>
2015-04-26 16:14:58 -0400

    Update cachematrix.R
    Added Comments to the makecachematrix function and laid out basic framework.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,27 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        ## create a null matrix
+        m <- NULL
+        
+        ## Set the cache matrix value using the set function
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        
+        ## Get the cache matrix value
+        get <- function() x
+        
+        ## Set the inverse of the matrix
+        setinverse <- function(inverse) m <<- inverse
+        
+        ## Get the inverse of the matrix
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+             
 }
 
 
